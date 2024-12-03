5b6beea3784e66611c04e4e3dfd08dd7e0d3f3b2
ramim141 <ahramu584@.com>
2024-12-03 23:06:23 +0600

    update

--- notessharing/settings.py
+++ notessharing/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'nssapp',
 ]
 
+# comment
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
